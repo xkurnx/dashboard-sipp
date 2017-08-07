86cc0008b0938156717ca83436a759fe8fa46295
xkurnx@gmail.com <xkurnx@gmail.com>
2017-08-07 11:14:19 +0700

    hmm.. lazy query

--- application/models/dashboard_model.php
+++ application/models/dashboard_model.php
@@ -9,7 +9,7 @@ class Dashboard_model extends CI_Model {
 	function fetch_upcoming_delegasi_keluar(){		
 			
 		$sql = "SELECT * FROM delegasi_keluar dk
-                WHERE  tgl_sidang + 1 > NOW() AND tgl_sidang < DATE_ADD( NOW(),INTERVAL 7 DAY) ";
+                 WHERE  tgl_sidang  > DATE_ADD( NOW(),INTERVAL -1 DAY) AND tgl_sidang < DATE_ADD( NOW(),INTERVAL 7 DAY) ORDER BY tgl_sidang ASC";
 		#	echo "<pre>$sql</pre>";
 		return $this->db->query($sql)->result_array();				
 	}
@@ -18,7 +18,7 @@ class Dashboard_model extends CI_Model {
 			
 		$sql = "SELECT * FROM delegasi_masuk dm LEFT OUTER JOIN delegasi_proses_masuk dpm
                 ON  dpm.`delegasi_id`=dm.`id`
-                WHERE  tgl_sidang + 1 > NOW() AND tgl_sidang < DATE_ADD( NOW(),INTERVAL 7 DAY)";
+                 WHERE  tgl_sidang  > DATE_ADD( NOW(),INTERVAL -1 DAY) AND tgl_sidang < DATE_ADD( NOW(),INTERVAL 7 DAY) ORDER BY tgl_sidang ASC";
 		#	echo "<pre>$sql</pre>";
 		return $this->db->query($sql)->result_array();				
 	}
